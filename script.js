5fbf53c4748c9fecb45f033ff1f75948c2a95442
Leo <santos-contato@hotmail.com.br>
2023-05-16 17:15:02 -0300

    alteracoes de script

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function calcularMediaValores() {
       soma += notas[i];
     }
     var media = soma / notas.length;
-    document.getElementById("resultado").innerHTML = "A média é: " + media.toFixed(2);
+    document.getElementById("resultado").innerHTML =
+      "A média é: " + media.toFixed(2);
   }
 
   var myModal = new bootstrap.Modal(document.getElementById("resultadoModal"), {
@@ -26,32 +27,30 @@ function calcularMediaValores() {
   myModal.show();
 }
 
-
-
 function aumentarForm() {
-  const container = document.querySelector('.calculadora-container');
-  const form = document.createElement('form');
-  form.classList.add('form-notaclass', 'mt-1');
+  const container = document.querySelector(".calculadora-container");
+  const form = document.createElement("form");
+  form.classList.add("form-notaclass", "mt-1");
   form.innerHTML = `
     <div class="flex-row form-group d-flex align-items-center justify-content-center">
       <div class="col-8 text-center">
         <input placeholder="Insira uma nota" type="number" step="1" class="form-control nota" id="nota" required>
       </div>
       <button type="button" class="btn btn-danger" onclick="excluirForm()">x</button>
-      <span class="m-1">Excluir</span>
     </div>
-  `;function calcularMediaValores() {
+  `;
+  function calcularMediaValores() {
     var notas = [];
     var soma = 0;
     var notasElementos = document.getElementsByClassName("nota");
-  
+
     for (var i = 0; i < notasElementos.length; i++) {
       var nota = parseFloat(notasElementos[i].value);
       if (!isNaN(nota)) {
         notas.push(nota);
       }
     }
-  
+
     if (notas.length === 0) {
       document.getElementById("resultado").innerHTML = "Nenhum valor informado";
     } else {
@@ -62,13 +61,16 @@ function aumentarForm() {
       document.getElementById("resultado").innerHTML =
         "A média é: " + media.toFixed(2);
     }
-  
-    var myModal = new bootstrap.Modal(document.getElementById("resultadoModal"), {
-      keyboard: false
-    });
+
+    var myModal = new bootstrap.Modal(
+      document.getElementById("resultadoModal"),
+      {
+        keyboard: false
+      }
+    );
     myModal.show();
   }
-  
+
   function aumentarForm() {
     const container = document.querySelector(".calculadora-container");
     const form = document.createElement("form");
@@ -83,12 +85,12 @@ function aumentarForm() {
     `;
     container.insertBefore(form, container.lastElementChild);
   }
-  
+
   function excluirForm() {
     const form = event.target.parentNode.parentNode;
     form.remove();
   }
-  
+
   container.insertBefore(form, container.lastElementChild);
 }
 
